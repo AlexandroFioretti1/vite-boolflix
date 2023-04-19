9a44c574fcd71c3809207268eb8a0d1f83c9301d
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-04-19 17:04:56 +0200

    fix code

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,13 @@
 <script>
 import { store } from "../assets/data/store";
 import SingleMovie from "./SingleMovie.vue";
+import CountryFlag from "vue-country-flag-next";
 
 export default {
   name: "AppMain",
   components: {
     SingleMovie,
+    CountryFlag,
   },
   data() {
     return {
@@ -17,6 +19,7 @@ export default {
 
 <template>
   <SingleMovie :movie="movie" v-for="movie in store.movies"></SingleMovie>
+
 </template>
 
 <style scoped lang="scss"></style>
