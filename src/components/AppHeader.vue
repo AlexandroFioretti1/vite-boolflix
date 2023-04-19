4cf743fd49ec9203edd815142172fcc6c1b90c98
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-04-19 12:18:57 +0200

    add header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,7 +12,17 @@ export default {
 </script>
 <template>
   <header>
-    <div>sono Header</div>
+    <div class="container">
+      <div class="d-flex justify-content-between align-items-center pt-2">
+        <h1>BOOLFLIX</h1>
+        <div>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Search Film or Series" aria-label="Search Film or Series" aria-describedby="button-addon2"/>
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2" > Search </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
